3842cef1ea4c15e653332378e3074ac8861a04b1
Jockey195 <136336256+Jockey195@users.noreply.github.com>
2024-01-26 21:53:56 +0300

    добавление кода в C#

--- SimpleCalculator/MainWindow.xaml.cs
+++ SimpleCalculator/MainWindow.xaml.cs
@@ -19,7 +19,7 @@ namespace SimpleCalculator
 
 
 
-        private void DigitButtons_Click (object sender, RoutedEventArgs e)
+        private void DigitButtons_Click(object sender, RoutedEventArgs e)
         {
             Button clickedButton = (Button)e.Source;
             string item = clickedButton.Content.ToString();
@@ -29,16 +29,23 @@ namespace SimpleCalculator
 
         private void btnDivide_Click(object sender, RoutedEventArgs e)
         {
-
+            _operation = Operation.Division;
+            _value1 = Convert.ToDecimal(_input);
+            _input = "";
         }
 
         private void btnMultiply_Click(object sender, RoutedEventArgs e)
         {
-
+            _operation = Operation.Multiplication;
+            _value1 = Convert.ToDecimal(_input);
+            _input = "";
         }
 
         private void btnMinus_Click(object sender, RoutedEventArgs e)
         {
+            _operation = Operation.Subtraction;
+            _value1 = Convert.ToDecimal(_input);
+            _input = "";
 
         }
 
@@ -47,6 +54,7 @@ namespace SimpleCalculator
             _operation = Operation.Addition;
             _value1 = Convert.ToDecimal(_input);
             _input = "";
+
         }
 
         private void btnCalculate_Click(object sender, RoutedEventArgs e)
@@ -58,9 +66,21 @@ namespace SimpleCalculator
                     _value2 = Convert.ToDecimal(_input);
                     result = _value1 + _value2;
                     break;
-
+                case Operation.Subtraction:
+                    _value2 = Convert.ToDecimal(_input);
+                    result = _value1 - _value2;
+                    break;
+                case Operation.Division:
+                    _value2 = Convert.ToDecimal(_input);
+                    result = _value1 / _value2;
+                    break;
+                case Operation.Multiplication:
+                    _value2 = Convert.ToDecimal(_input);
+                    result = _value1 * _value2;
+                    break;
+                default:
+                    break;
             }
-
             _input = "";
             txtOutput.Text = result.ToString();
         }
